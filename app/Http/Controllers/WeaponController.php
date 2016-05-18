7f9e214a6db8538cdd1535603adab6443dca1070
capitaladot <austinitor@gmail.com>
2016-05-17 21:36:33 -0400

    Unadded file.

---
+++ app/Http/Controllers/WeaponController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Repositories\WeaponRepository;
+use Illuminate\Routing\Route;
+
+class WeaponController extends BaseController {
+	public function __construct(WeaponRepository $repository) {
+		$this->repository = $repository;
+		parent::__construct ();
+	}
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index(Route $route) {
+		$all = $this->repository->all ();
+		return view ( 'items/index', [ 
+				'modelName' => strtolower ( class_basename ( $this->repository->model () ) ),
+				'models' => $all ? $all : [ ],
+				'route' => $route 
+		] );
+	}
+	public function show($idOrSlug, Route $route) {
+		$show = $this->repository->find ( $idOrSlug );
+		if(!$show)
+			return response('Not found.',404);
+		$show->provideRelatables ();
+		$show->getProcessedFillables ();
+		
+		return view ( 'items/show', [ 
+			'route' => $route,
+			'modelName' => $this->modelName,
+			'baseUrl'=>$this->baseUrl,
+			'model' => $show,
+			'edit' => $show->getUrl () . '/edit',
+			'table' => $show->getTable (),
+			'hidden' => $show->getHidden (),
+			'fillables' => $show->processedFillables,
+			'relationControls' => $show->relationControls,
+			'relationMethods' => $show->relationMethods,
+			'title' => isset ( $show->traits ['Navigatable'] ) ? $show->getTitle () : '' 
+		] );
+	}
+}
