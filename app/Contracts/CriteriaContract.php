59b01df9faaac199bea26fb1c5df54899badf551
capitaladot <austinitor@gmail.com>
2016-05-18 10:00:19 -0400

    Unadded file.

---
+++ app/Contracts/CriteriaContract.php
@@ -0,0 +1,37 @@
+<?php namespace App\Contracts;
+ 
+use App\Criteria\Criteria;
+ 
+/** 
+ * Interface CriteriaInterface
+ */
+interface CriteriaContract {
+ 
+    /**
+     * @param bool $status
+     * @return $this
+     */
+    public function skipCriteria($status = true);
+ 
+    /**
+     * @return mixed
+     */
+    public function getCriteria();
+ 
+    /**
+     * @param Criteria $criteria
+     * @return $this
+     */
+    public function getByCriteria(Criteria $criteria);
+ 
+    /**
+     * @param Criteria $criteria
+     * @return $this
+     */
+    public function pushCriteria(Criteria $criteria);
+ 
+    /**
+     * @return $this
+     */
+    public function  applyCriteria();
+}
