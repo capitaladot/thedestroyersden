526ba8dc6454774eea99bd862c85cf7b8936b5b3
capitaladot <austinitor@gmail.com>
2016-05-17 21:34:16 -0400

    Uncommitted files.

---
+++ config/google.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+    |----------------------------------------------------------------------------
+    | Google application name
+    |----------------------------------------------------------------------------
+    */
+    'application_name' => "The Destroyer's Den",
+
+    /*
+    |----------------------------------------------------------------------------
+    | Google OAuth 2.0 access
+    |----------------------------------------------------------------------------
+    |
+    | Keys for OAuth 2.0 access, see the API console at
+    | https://developers.google.com/console
+    |
+    */
+    'client_id'       => ***REMOVED***,
+    /* | Path to key file
+    | Example storage_path().'/key/google.p12'
+    */
+    'key_file_location' => "/key/TheDestroyersDen-8857206f6a2a.p12",
+    /**/
+    'service_account_name' =>"***REMOVED***"
+
+];
